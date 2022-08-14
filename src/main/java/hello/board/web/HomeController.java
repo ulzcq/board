b808fd1496b3af03be0cf1934ea0e2dfb4575cef
ulzcq <syj9913@ynu.ac.kr>
2022-08-14 22:43:01 +0900

    Feat: 홈 컨트롤러 추가, 누락 클래스(세션 상수) 추가

---
+++ src/main/java/hello/board/web/HomeController.java
@@ -0,0 +1,33 @@
+package hello.board.web;
+
+import hello.board.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    /** 홈 화면
+     * 로그인 하지 않은 사용자 -> home
+     * 로그인 한 사용자 -> loginHome
+     * */
+    @GetMapping("/")
+    public String Home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+                               Member loginMember, Model model){
+
+        //세션 가져오는 건 스프링이 해준다
+        //로그인 세션이 없는 사용자는 홈으로 돌려보낸다
+        if(loginMember == null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인 홈으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+}
