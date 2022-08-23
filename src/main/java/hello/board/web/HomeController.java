2e4de69f9dcdfe8b0a37bf752b8c572c981917ef
ulzcq <syj9913@ynu.ac.kr>
2022-08-23 23:54:43 +0900

    Fix: HomeController 변경 - 로그인한 사용자는 회원용 게시판으로

--- src/main/java/hello/board/web/HomeController.java
+++ src/main/java/hello/board/web/HomeController.java
@@ -1,5 +1,6 @@
 package hello.board.web;
 
+import hello.board.SessionConst;
 import hello.board.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,11 +14,11 @@ public class HomeController {
 
     /** 홈 화면
      * 로그인 하지 않은 사용자 -> home
-     * 로그인 한 사용자 -> loginHome
+     * 로그인 한 사용자 -> 회원용 게시판
      * */
     @GetMapping("/")
-    public String Home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
-                               Member loginMember, Model model){
+    public String Home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+                       Model model){
 
         //세션 가져오는 건 스프링이 해준다
         //로그인 세션이 없는 사용자는 홈으로 돌려보낸다
@@ -25,9 +26,9 @@ public class HomeController {
             return "home";
         }
 
-        //세션이 유지되면 로그인 홈으로 이동
-        model.addAttribute("member", loginMember);
-        return "loginHome";
+        //세션이 유지되면 게시판(구:로그인 홈)으로 이동
+//        model.addAttribute("member", loginMember);
+        return "redirect:/posts";
     }
 
 }
