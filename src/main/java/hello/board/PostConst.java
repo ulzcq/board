e1aa52513da9d0839be4bb82425e502dd5a6ed3e
ulzcq <syj9913@ynu.ac.kr>
2022-09-07 18:39:04 +0900

    Feat: 게시글 상수클래스 누락된거 추가

---
+++ src/main/java/hello/board/PostConst.java
@@ -0,0 +1,7 @@
+package hello.board;
+
+public interface PostConst {
+    String FILE_SIZE_EXCEEDED = "파일당 10MB까지 첨부 가능합니다.";
+    int MB_TO_BYTE = 1048576; //1MB = 1024 * 1024 byte
+    int KB_TO_BYTE = 1024;
+}
